ab9015fe37d873291495bf3d6356aa8a64288bd0
Faiz <mohdfaiz182005@gmail.com>
2025-07-21 15:24:36 +0530

    Added Local Storage

--- script.js
+++ script.js
@@ -11,32 +11,80 @@ cancelBtn.addEventListener("click", () => {
 function save() {
   let inputNote = document.getElementById("input-note").value;
   let inputDes = document.getElementById("input-des").value;
+
   if (inputNote === "" || inputDes === "") {
     alert("Enter a Valid Note");
     return null;
   }
-  let container = document.querySelector("section");
-  let note = document.createElement("div");
-  note.innerHTML = `<div class="card">
-  <div class="card-header">
-    <h2>${inputNote}</h2>
-    <button class="close-btn">X</button>
-  </div>
-  <p>
-  ${inputDes}</p>
-</div>`;
-  let card = note.firstElementChild;
-  container.append(card);
+
+  const noteObj = {
+    id: Date.now(),
+    title: inputNote,
+    description: inputDes,
+  };
+
+  let notesArr = JSON.parse(localStorage.getItem("notes")) || [];
+
+  notesArr.push(noteObj);
+
+  localStorage.setItem("notes", JSON.stringify(notesArr));
+  createCard(noteObj);
+
   document.getElementById("input-note").value = "";
   document.getElementById("input-des").value = "";
   dialog.classList.remove("active");
+}
+
+function createCard(noteObj) {
+  console.log(noteObj);
+  let container = document.querySelector("section");
+  let note = document.createElement("div");
+
+  note.innerHTML = `<div class="card" data-id="${noteObj.id}">
+  <div class="card-header">
+    <h2>${noteObj.title}</h2>
+    <button class="close-btn">X</button>
+  </div>
+  <p>
+  ${noteObj.description}</p>
+</div>`;
+
+  let card = note.firstElementChild;
+  container.append(card);
+  checkEmptyMessage();
 }
 
 document.querySelector("section").addEventListener("click", (e) => {
   if (e.target.classList.contains("close-btn")) {
     const card = e.target.closest(".card");
     if (card) {
+      const cardId = card.getAttribute("data-id");
+      let notes = JSON.parse(localStorage.getItem("notes")) || [];
+      notes = notes.filter((note) => note.id != cardId);
+
+      localStorage.setItem("notes", JSON.stringify(notes));
+
       card.remove();
+      checkEmptyMessage();
     }
   }
 });
+
+window.addEventListener("DOMContentLoaded", () => {
+  const notesArr = JSON.parse(localStorage.getItem("notes")) || [];
+  notesArr.forEach((elem) => {
+    createCard(elem);
+    checkEmptyMessage();
+  });
+});
+
+function checkEmptyMessage() {
+  const notes = JSON.parse(localStorage.getItem("notes")) || [];
+  const msg = document.getElementById("empty-msg");
+
+  if (notes.length === 0) {
+    msg.style.display = "block";
+  } else {
+    msg.style.display = "none";
+  }
+}
